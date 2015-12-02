02d53ad8060a707618c5058b0999e2fbc2320207
demsking <sebastien@campusjeunes.net>
2015-12-02 23:59:16 +0100

    Initial commit

--- index.js
+++ index.js
@@ -26,26 +26,38 @@ var stream = function(buffer) {
     });
 };
 
-module.exports = function() {    
+module.exports = function(options) {    
+    options = options || {};
+    
     return stream(function(file_contents) {
         var $ = cheerio.load(file_contents);
         var selector_cmds = {'move-before': 'before', 'move-to': 'append', 'move-after': 'after'};
         
         for (var selector in selector_cmds) {
-            $('[' + selector + ']').each(function() {
-                var selector_target = $(this).attr(selector);
-                
-                if (selector_target.length) {
-                    var tag = $(this).clone();
+            try {
+                $('[' + selector + ']').each(function() {
+                    var selector_target = $(this).attr(selector);
                     
-                    $(this).remove();
-                    tag.removeAttr(selector);
-                    
-                    $(selector_target)[selector_cmds[selector]](tag);
-                }
-            });
+                    if (selector_target.length) {
+                        var tag = $(this).clone();
+                        
+                        if ($(selector_target)[selector_cmds[selector]](tag).length) {
+                            $(this).remove();
+                            tag.removeAttr(selector);
+                        }
+                    }
+                });
+            } catch(err) {
+                throw new gutil.PluginError('gulp-move-tags', err);
+            }
+        }
+        
+        var output = $.html();
+        
+        if (typeof options.done == 'function') {
+            options.done(output);
         }
 
-        return $.html();
+        return output;
     });
 };
